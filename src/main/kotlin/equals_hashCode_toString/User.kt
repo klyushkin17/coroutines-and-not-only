d65e5d0242f6edc427894be81deb4d91119169ee
klyushkin <stepan.9h88@mail.ru>
2024-07-11 00:08:39 +0300

    equals(). hashCode()

--- src/main/kotlin/equals_hashCode_toString/User.kt
+++ src/main/kotlin/equals_hashCode_toString/User.kt
@@ -1,8 +1,23 @@
 package equals_hashCode_toString
 
-class User (var firstName: String, var lastName: String, var age: Int){
+data class User (var passportNumber: String, var lastName: String, var age: Int){
 
     override fun toString(): String {
-        return "First name: $firstName, Last name: $lastName, Age: $age"
+        return "First name: $passportNumber, Last name: $lastName, Age: $age"
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as User
+
+        return passportNumber == other.passportNumber
+    }
+
+    override fun hashCode(): Int {
+        return passportNumber.hashCode()
+    }
+
+
 }
--- src/main/kotlin/equals_hashCode_toString/toString.kt
+++ src/main/kotlin/equals_hashCode_toString/toString.kt
@@ -1,6 +1,10 @@
 package equals_hashCode_toString
 
 fun main() {
-    val user1 = User("John", "Doe", 25)
-    println(user1.toString())
+    val user1 = User("123456", "Doe", 25)
+    val user2 = User("123456", "Doe1", 30)
+
+    println(user1 == user2)
+    println(user1.hashCode())
+    println(user2.hashCode())
 }
