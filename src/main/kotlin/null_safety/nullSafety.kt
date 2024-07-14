ac23402dca3610c471f8fb27df7d8833219422b7
klyushkin <stepan.9h88@mail.ru>
2024-07-15 00:50:44 +0300

    let, also, apply, run, with

--- src/main/kotlin/null_safety/nullSafety.kt
+++ src/main/kotlin/null_safety/nullSafety.kt
@@ -4,16 +4,20 @@ fun main() {
     val answer = getAnswer("Any question")
 
     // !!. - мы уверены, что answer не будет равен null и функция toUpperCase() вызовиться в любом случае
-    /*println(answer!!.toUpperCase())*/
+/*println(answer!!.toUpperCase())
+
 
     // ?. - мы не уверены, что answer не будет равен null и функция to UpperCase() не вызовиться, если answer == null
-    /*println(answer?.toUpperCase())*/
+println(answer?.toUpperCase())
+
 
     // ?: - так можно выкинуть ошибку в случает null
-    /*println(answer?:throw UserNotFound())*/
+println(answer?:throw UserNotFound())
+
 
     // ?: - так можно подсавить дефолтное значение в случае null
-    /*println(answer?: "Default answer")*/
+println(answer?: "Default answer")*/
+
 }
 
 
@@ -26,3 +30,4 @@ fun getAnswer(question: String): String? {
 
     return answer
 }
+
