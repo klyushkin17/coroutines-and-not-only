10773da6ef27af529b05dec552e178342b5a3dcd
klyushkin <stepan.9h88@mail.ru>
2024-09-09 00:01:14 +0300

    coroutines

--- src/main/kotlin/coroutines/practice.kt
+++ src/main/kotlin/coroutines/practice.kt
@@ -1,6 +1,12 @@
 package coroutines
 
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import kotlin.coroutines.EmptyCoroutineContext
+
+var counter = 0
 
 suspend fun main(): Unit = coroutineScope {
     val result: Deferred<String> = CoroutineScope(Dispatchers.IO).async {
@@ -79,7 +85,115 @@ suspend fun main(): Unit = coroutineScope {
                 }
             }
         }
+
+        // Как поймать исключение
+
+        // 1) try/catch
+        CoroutineScope(context = EmptyCoroutineContext).launch {
+            launch {
+                try {
+                    delay(100L)
+                    throw RuntimeException()
+                } catch (e: Exception){
+                    // Exception handling
+                }
+            }
+        }
+
+        // 2) runCaching
+        CoroutineScope(context = EmptyCoroutineContext).launch {
+            launch {
+                val result: Result<Unit> = kotlin.runCatching {
+                    delay(100L)
+                    throw RuntimeException()
+                }
+            }
+        }
+
+        // 3) CoroutineExceptionHandler
+
+        CoroutineScope(context = EmptyCoroutineContext).launch {
+            val handler = CoroutineExceptionHandler { _, exception ->
+                println("Exception $exception handled")
+            }
+
+            launch(SupervisorJob() + handler) {
+                try {
+                    delay(100L)
+                    throw RuntimeException("bruh")
+                } catch (e: Exception){
+                    // Exception handling
+                }
+            }
+        }
     }
     job.start()
     job.cancel()
+
+
+    // Share States
+    massiveRun { counter++ }
+    println(counter)
+
+    // Channel - необходим для передачи данных между корутинами
+
+    supervisorScope {
+
+        val channel: Channel<String> = Channel<String>()
+
+        //sender
+        launch {
+            repeat(10) { index ->
+                channel.send("send index $index")
+            }
+            channel.close()
+        }
+
+        //receiver
+        launch {
+            for (message in channel) {
+                println(message)
+            }
+        }
+
+        //flow collection
+        launch {
+            emitToFlow().collect { data ->
+                println(data)
+
+            }
+        }
+    }
+
+
+    //Разница между flow и channel в том, что
+    // Channel - генерирует данные даже если нет получается
+    // Flow - генерирует данные только если есть получатель
+}
+
+suspend fun massiveRun(action: suspend () -> Unit) {
+    val mutex = Mutex()
+
+    withContext(Dispatchers.Main) {
+        repeat(100) {
+            launch {
+                repeat(100) {
+                    mutex.withLock {
+                        action()
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Для решения проблемы с доступом можно использовать: Atomic, coroutine mutex, Thread confinement (Использование одного потока для измеения данных)
+
+
+suspend fun emitToFlow(): Flow<String> {
+    return flow {
+        repeat(2) { index ->
+            emit(index.toString())
+        }
+    }
 }
